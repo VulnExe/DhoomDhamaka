8623225acfafc6d5728655ac2f450caad41359ab
Jerry <jerryjvj33@gmail.com>
2022-06-10 22:21:35 +0530

    machanae

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-backend file  simple work
-=======
-//this is server file
-<<<<<<< HEAD
->>>>>>> 967c5082e6715bfb863b8fb46c54c97c4cbedf9d
-
-
-=======
-nfkweflsdjl
->>>>>>> b0ea81d0208f05ab64722d30fb0688daa2c47f30
-
-machjaa
+hello
